967eab353b3bc51dac82bd0bfb80beb7e1084fd8
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-10 19:11:19 +0100

    ciclo for per img

--- js/app.js
+++ js/app.js
@@ -21,4 +21,25 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
-console.log(items, title, text);
+//console.log(items, title, text);
+
+// recupero dal DOM il contenitore delle immagini
+
+const slideWrapper = document.querySelector('.slide-wrapper');
+//console.log(slideWrapper);
+
+// creo il ciclo per le immagini del carusel
+
+for (let i = 0; i < items.length; i++) {
+
+    // creo l'elemento che andrà nel DOM 
+
+    const item = document.createElement('div');
+    console.log(item);
+    const img = document.createElement('img');
+    console.log(img);
+
+    item.classList.add('item');
+
+
+}
